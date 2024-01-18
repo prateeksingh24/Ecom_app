94525c79469b0495454009533630714823a57d4f
prateeksingh24 <prateeksingh2423@gmail.com>
2024-01-19 01:43:34 +0530

    first commit

--- lib/componenets/my_drawer.dart
+++ lib/componenets/my_drawer.dart
@@ -7,7 +7,7 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
+
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
         children: [
