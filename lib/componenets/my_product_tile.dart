c73a39c7eaa854326236a82e8c489e05d64c4061
prateeksingh24 <prateeksingh2423@gmail.com>
2024-01-25 22:11:32 +0530

    Day3

--- lib/componenets/my_product_tile.dart
+++ lib/componenets/my_product_tile.dart
@@ -8,20 +8,67 @@ class MyProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(25),
+      width: 300,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          //product image
-          Icon(Icons.favorite),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //product image
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    width: double.infinity,
+                    padding: EdgeInsets.all(25),
+                    child: Icon(Icons.favorite)),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
 
-          //product name
-          Text(product.name),
-          //product description
-          
-          Text(product.description),
+              //product name
+              Text(
+                product.name,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              //product description
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              Text(
+                product.description,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ],
+          ),
 
           //product price+add to cart button
-          
-          Text(product.price.toStringAsFixed(2))
+          const SizedBox(
+            height: 25,
+          ),
+
+          Row(
+            children: [
+              Text(product.price.toStringAsFixed(2)),
+            ],
+          )
         ],
       ),
     );
