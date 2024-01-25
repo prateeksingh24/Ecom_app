cd1b912e6a490e44bde9392659b20d5b096c878d
prateeksingh24 <prateeksingh2423@gmail.com>
2024-01-25 18:59:24 +0530

    second commit

---
+++ lib/models/shop.dart
@@ -0,0 +1,51 @@
+import 'package:minimal_ecom/models/product.dart';
+
+class Shop {
+  //products for sale
+  final List<Product> _shop = [
+    //product 1
+    Product(
+        name: "Product 1",
+        price: 99.99,
+        description: "Item Description",
+        // imagePath: imagePath
+    ),
+    Product(
+      name: "Product 2",
+      price: 99.99,
+      description: "Item Description",
+      // imagePath: imagePath
+    ),
+    Product(
+      name: "Product 3",
+      price: 99.99,
+      description: "Item Description",
+      // imagePath: imagePath
+    ),
+    Product(
+      name: "Product 4",
+      price: 99.99,
+      description: "Item Description",
+      // imagePath: imagePath
+    ),
+  ];
+
+  //user cart
+List <Product> _cart = [];
+
+  //get product list
+List <Product> get shop => _shop;
+
+  //get user cart
+List <Product> get cart => _cart;
+
+  //add item to cart
+void addToCart(Product item){
+  _cart.add(item);
+}
+
+  //remove item from cart
+void removeFromCart(Product item){
+  _cart.remove(item);
+}
+}
