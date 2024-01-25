59038e0e55c3e1268cdc5ee78c84aecab6fadb3a
prateeksingh24 <prateeksingh2423@gmail.com>
2024-01-25 22:11:32 +0530

    Day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_ecom/models/shop.dart';
 import 'package:minimal_ecom/pages/cart_page.dart';
 import 'package:minimal_ecom/pages/intro_page.dart';
 import 'package:minimal_ecom/pages/shop_page.dart';
 import 'package:minimal_ecom/themes/light_mode.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMode,
-      home: const IntroPage(),
+      initialRoute: '/intro_page',
       routes: {
-        '/intro_page':(context)=>const IntroPage(),
-        '/shop_page':(context)=>const ShopPage(),
-        '/cart_page':(context)=> const CartPage(),
+        '/intro_page': (context) => const IntroPage(),
+        '/shop_page': (context) => const ShopPage(),
+        '/cart_page': (context) => const CartPage(),
       },
     );
   }
 }
-
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_ecom/componenets/my_drawer.dart';
+import 'package:minimal_ecom/componenets/my_product_tile.dart';
+import 'package:minimal_ecom/models/product.dart';
+import 'package:minimal_ecom/models/shop.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final products = context.watch<Shop>().shop;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -15,7 +21,41 @@ class ShopPage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MyDrawer(),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          //shop title
+          Center(
+            child: Text(
+              "Pick from a selected list of premium products",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+          ),
 
+          //product subtitle
+
+          //product list
+          SizedBox(
+            height: 550,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: products.length,
+              padding: EdgeInsets.all(15),
+              itemBuilder: (context, index) {
+                //get each individual products from shop
+                final product = products[index];
+
+                //return as a product title Ui
+                return MyProductTile(product: product);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
