bcd7e5e01e698172ee72b94a85dae5d3a5a3adc2
prateeksingh24 <prateeksingh2423@gmail.com>
2024-01-25 19:29:50 +0530

    second commit

---
+++ lib/componenets/my_product_tile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_ecom/models/product.dart';
+
+class MyProductTile extends StatelessWidget {
+  final Product product;
+  const MyProductTile({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          //product image
+          Icon(Icons.favorite),
+
+          //product name
+          Text(product.name),
+          //product description
+          
+          Text(product.description),
+
+          //product price+add to cart button
+          
+          Text(product.price.toStringAsFixed(2))
+        ],
+      ),
+    );
+  }
+}
